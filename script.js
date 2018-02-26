2c80967600aa2572f57b91c4c1a56d3dd56e94fa
Jake Berg <jacobvictorberg@gmail.com>
2018-02-26 12:18:23 -0500

    now shows how you win  with directions

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function connectFour(){
+function connectFour() {
 
-const destination = document.getElementById("board");
-let clickCount = 0;
+    const destination = document.getElementById("board");
+    let clickCount = 0;
 
-const gameArray = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0]
-]
+    const gameArray = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
 
-//Creates game board
+    //Creates game board
     for (let i = 0; i < gameArray.length; i++) {
         let column = document.createElement("div");
         column.classList.add('column');
@@ -22,134 +22,138 @@ const gameArray = [
     }
 
 
-// Fixes column id so it can be used in function
-function prepareId(p) {
-    let noDash = p.split("-");
-    let removeLetters = noDash.pop();
-    return removeLetters;
-}
-
-// Adds a black or red div on the game board
-const placeChip = function () {
-    let thisCol = this;
-    let idNum = prepareId(thisCol.id);
-
-    let x = document.getElementById("col-" + idNum).childElementCount;
-    let y = 5;
-
-    if (clickCount % 2 && x < y + 1) {
-        let redDisc = document.createElement("div");
-        redDisc.className = ("red");
-        thisCol.appendChild(redDisc);
-        gameArray[idNum][y - x] = 1;
-    } else if (x < y + 1) {
-        let blackDisc = document.createElement("div");
-        blackDisc.className = ("black");
-        thisCol.appendChild(blackDisc);
-        gameArray[idNum][y - x] = 2;
+    // Fixes column id so it can be used in function
+    function prepareId(p) {
+        let noDash = p.split("-");
+        let removeLetters = noDash.pop();
+        return removeLetters;
     }
-    clickCount++
-}
 
-// This checks vertically in the game and horizontally in the array
-const checkWinVertical = function () {
-    for (let y = 0; y < gameArray.length; y++) {
-        let row = gameArray[y];
+    // Adds a black or red div on the game board
+    const placeChip = function () {
+        let thisCol = this;
+        let idNum = prepareId(thisCol.id);
 
-        for (let x = 0; x < row.length; x++) {
-            let cell = row[x];
-            if (cell !== 0) {
-                if (cell === gameArray[y][x + 1] && cell === gameArray[y][x + 2] && cell === gameArray[y][x + 3]) {
-                    alert('You win!');
-                    reset();
-                    connectFour();
+        let x = document.getElementById("col-" + idNum).childElementCount;
+        let y = 0;
+
+        if (clickCount % 2 && x < 6) {
+            let redDisc = document.createElement("div");
+            redDisc.className = ("red");
+            thisCol.appendChild(redDisc);
+            gameArray[idNum][y + x] = 1;
+        } else if (x < 6) {
+            let blackDisc = document.createElement("div");
+            blackDisc.className = ("black");
+            thisCol.appendChild(blackDisc);
+            gameArray[idNum][y + x] = 2;
+        }
+        clickCount++
+        console.log(gameArray)
+    }
+
+    // This checks vertically in the game and horizontally in the array
+    const checkWinVertical = function () {
+        for (let y = 0; y < gameArray.length; y++) {
+            let row = gameArray[y];
+
+            for (let x = 0; x < row.length; x++) {
+                let cell = row[x];
+                if (cell !== 0) {
+                    if (cell === gameArray[y][x + 1] && cell === gameArray[y][x + 2] && cell === gameArray[y][x + 3]) {
+                        return true;
+                        alert('You win vertically!');
+                        reset();
+                        connectFour();
+                    }
                 }
             }
         }
     }
-}
 
-// This checks horizontally in the game and vertically in the array
-const checkWinHorizontal = function () {
-    for (let y = 0; y < gameArray.length; y++) {
-        let row = gameArray[y];
 
-        for (let x = 0; x < row.length; x++) {
-            let cell = gameArray[y][x];
 
-            if (cell !== 0) {
-                if (cell === gameArray[y + 1][x] && cell === gameArray[y + 2][x] && cell === gameArray[y + 3][x]) {
-                    alert('You win!');
-                    reset();
-                    connectFour();
+    // This checks horizontally in the game and vertically in the array
+    const checkWinHorizontal = function () {
+        for (let y = 0; y < gameArray.length; y++) {
+            let row = gameArray[y];
+
+            for (let x = 0; x < row.length; x++) {
+                let cell = gameArray[y][x];
+
+                if (cell !== 0) {
+                    if (cell === gameArray[y + 1][x] && cell === gameArray[y + 2][x] && cell === gameArray[y + 3][x]) {
+                        alert('You win horizontal!');
+                        reset();
+                        connectFour();
+                    }
                 }
             }
         }
     }
-}
 
-// Checks down and to the right for diagonal win
-const checkWinDiagonalRight = function () {
-    for (let y = 0; y < gameArray.length; y++) {
-        let row = gameArray[y];
-        // iterate each cell in the row
-        for (let x = 0; x < row.length; x++) {
-            cell = gameArray[y][x];
+    // Checks down and to the right for diagonal win
+    const checkWinDiagonalRight = function () {
+        for (let y = 0; y < gameArray.length; y++) {
+            let row = gameArray[y];
+            // iterate each cell in the row
+            for (let x = 0; x < row.length; x++) {
+                cell = gameArray[y][x];
 
-            // Only check if cell is filled
-            if (cell !== 0) {
+                // Only check if cell is filled
+                if (cell !== 0) {
 
-                // Check the next two cells for the same value
-                if (cell === gameArray[y + 1][x + 1] && cell === gameArray[y + 2][x + 2] && cell === gameArray[y + 3][x + 3]) {
-                    alert('You win!');
-                    reset();
-                    connectFour();
+                    // Check the next two cells for the same value
+                    if (cell === gameArray[y - 1][x + 1] && cell === gameArray[y - 2][x + 2] && cell === gameArray[y - 3][x + 3]) {
+                        alert('You win down right!');
+                        reset();
+                        connectFour();
+                    }
                 }
             }
         }
     }
-}
-// Checks down and to the left for diagonal win
-const checkWinDiagonalLeft = function () {
-    for (let y = 0; y < gameArray.length; y++) {
-        let row = gameArray[y];
-        // iterate each cell in the row
-        for (let x = 0; x < row.length; x++) {
-            cell = gameArray[y][x];
+    // Checks down and to the left for diagonal win
+    const checkWinDiagonalLeft = function () {
+        for (let y = 0; y < gameArray.length; y++) {
+            let row = gameArray[y];
+            // iterate each cell in the row
+            for (let x = 0; x < row.length; x++) {
+                cell = gameArray[y][x];
 
-            // Only check if cell is filled
-            if (cell !== 0) {
+                // Only check if cell is filled
+                if (cell !== 0) {
 
-                // Check the next two cells for the same value
-                if (cell === gameArray[y - 1][x + 1] && cell === gameArray[y - 2][x + 2] && cell === gameArray[y - 3][x + 3]) {
-                    alert('You Win!');
-                    reset();
-                    connectFour();
+                    // Check the next two cells for the same value
+                    if (cell === gameArray[y + 1][x + 1] && cell === gameArray[y + 2][x + 2] && cell === gameArray[y + 3][x + 3]) {
+                        alert('You Win down left!');
+                        reset();
+                        connectFour();
+                    }
                 }
             }
         }
     }
-}
 
-function reset() {
-    let myNode = document.getElementById("board");
-    while (myNode.firstChild) {
-        myNode.removeChild(myNode.firstChild);
+    function reset() {
+        let myNode = document.getElementById("board");
+        while (myNode.firstChild) {
+            myNode.removeChild(myNode.firstChild);
+        }
     }
-}
 
 
-// Click event for choices.
-var columns = document.querySelectorAll(".column");
+    // Click event for choices.
+    var columns = document.querySelectorAll(".column");
 
-for (let i = 0; i < columns.length; i++) {
-    let column = columns[i];
-    column.addEventListener('click', placeChip);
-    column.addEventListener('click', checkWinVertical);
-    column.addEventListener('click', checkWinHorizontal);
-    column.addEventListener('click', checkWinDiagonalRight);
-    column.addEventListener('click', checkWinDiagonalLeft);
-}
+    for (let i = 0; i < columns.length; i++) {
+        let column = columns[i];
+        column.addEventListener('click', placeChip);
+        column.addEventListener('click', checkWinVertical);
+        column.addEventListener('click', checkWinHorizontal);
+        column.addEventListener('click', checkWinDiagonalRight);
+        column.addEventListener('click', checkWinDiagonalLeft);
+    }
 
 }
 
